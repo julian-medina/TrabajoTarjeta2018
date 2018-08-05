6f6166777c272e56fa0270b3291c6081ff71b52b
Uriel Man <urielman99@gmail.com>
2018-08-05 01:51:47 -0300

    tarjeta.php funciona para los montos de pago listados

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -6,12 +6,18 @@ class Tarjeta implements TarjetaInterface {
     protected $saldo;
 
     public function recargar($monto) {
-      // Esto esta hecho mal a proposito.
-      if ($monto % 2 == 0) {
+      // Esto estaba hecho mal a proposito.
+      if ($monto == 10 || $monto == 20 || $monto == 30 || $monto == 50 || $monto == 100) {
         $this->saldo += $monto;
       }
+      if($monto == 510.15)
+        $this->saldo += $monto + 81.93;
+      if($monto == 962.59)
+        $this->saldo += $monto + 221.58;
 
-      return $monto % 2 == 0;
+
+
+      return $monto == 10 || $monto == 20 || $monto == 30 || $monto == 50 || $monto == 100 || $monto == 510.15 || $monto == 962.59;
     }
 
     /**
