c866d85f4b0225cbf9e4dc7940b6587d3d32a98f
Uriel Man <urielman99@gmail.com>
2018-08-05 02:48:04 -0300

    cree la clase Colectivo y sus funciones

---
+++ src/Colectivo.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace TrabajoTarjeta;
+
+class Colectivo implements ColectivoInterface {
+
+    protected $linea;
+
+    protected $empresa;
+
+    protected $numero;
+
+    public function linea(){
+        return $this->linea;
+    }
+
+    public function empresa(){
+        return $this->empresa;
+    }
+
+    public function numero(){
+        return $this->numero;
+    }
+
+    /**
+     * Paga un viaje en el colectivo con una tarjeta en particular.
+     *
+     * @param TarjetaInterface $tarjeta
+     *
+     * @return BoletoInterface|FALSE
+     *  El boleto generado por el pago del viaje. O FALSE si no hay saldo
+     *  suficiente en la tarjeta.
+     */
+    public function pagarCon(TarjetaInterface $tarjeta){
+        $valor = 14.80;
+        $boleto = new Boleto($valor, $this, $tarjeta);
+        if($tarjeta->saldo >= $valor){
+            $tarjeta->saldo -= $valor;
+            return $boleto;
+        }
+        else {
+            return False;
+        }
+    }
+}
