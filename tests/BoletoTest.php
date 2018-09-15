847e5d56ea5352be476872a90193aabea6461571
Uriel Man <urielman99@gmail.com>
2018-09-15 20:26:06 -0300

    tests adicionales en boleto

--- tests/BoletoTest.php
+++ tests/BoletoTest.php
@@ -7,9 +7,20 @@ use PHPUnit\Framework\TestCase;
 class BoletoTest extends TestCase {
 
     public function testSaldoCero() {
-        $valor = 14.80;
-        $boleto = new Boleto($valor, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+        $tiempo = new TiempoFalso();
+        $id = "123456";
+        $tarjeta = new Tarjeta($tiempo, $id);
+    
+        $colectivo = new Colectivo(NULL, NULL, NULL);
+            
+        $boleto = new Boleto($tarjeta->valorBoleto(), $colectivo, $tarjeta, date("d/m/y H:i", time()), get_class($tarjeta), $tarjeta->obtenerSaldo(), $tarjeta->obtenerId(), 0);
+        
 
-        $this->assertEquals($boleto->obtenerValor(), $valor);
+        $this->assertEquals($boleto->obtenerValor(), $tarjeta->valorBoleto());
+        $this->assertEquals($boleto->obtenerColectivo(), $colectivo);
+        $this->assertEquals($boleto->obtenerFecha(), date("d/m/y H:i", time()));
+        $this->assertEquals($boleto->obtenerTipoTarjeta(), get_class($tarjeta));
+        $this->assertEquals($boleto->obtenerSaldo(), $tarjeta->obtenerSaldo());
+        $this->assertEquals($boleto->obtenerId(), $tarjeta->obtenerId());
     }
 }
