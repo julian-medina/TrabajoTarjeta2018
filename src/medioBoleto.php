c1d9c9d7ef9f8efd30acf96fe5f773787c148e55
Uriel Man <urielman99@gmail.com>
2018-09-15 21:00:58 -0300

    eliminado archivo obsoleto

--- src/medioBoleto.php
+++ src/medioBoleto.php
@@ -1,38 +1,10 @@
 <?php
 
 namespace TrabajoTarjeta;
-class MedioBoletoEstudiantil extends Tarjeta implements TarjetaInterface{
-    
-    protected $tiempoMedioInusable = 300;
-    protected $momentoPagado = NULL;
-    
-    public function medioDisponible(){
-        $fecha_ultima = $this->obtenerMomentoPagado();
-       
-   public function obtenerCooldown(){
-          return $this->tiempoMedioInusable;
-      }
-      
-   public function obtenerMomentoPagado(){
-        return $this->momentoPagado;
-      }
-      
-   public function obtenerValorMedio(){
-        return $this->valor_boleto/2; public function obtenerValorMedio(){
-        return $this->valor_boleto/2;
-      }
-    
-   public function pagar() {
-        if($this->saldo >= $this->obtenerValorMedio()){
-            if($this->momentoPagado == NULL || $this->medioDisponible()){
-                $this->saldo -= $this->obtenerValorMedio();
-                $timer = $this->tiempo->time();
-                $this->momentoPagado = $timer;
-            }
-            else{
-                return FALSE;
-            }
-            return TRUE;
-        }
-        
+
+/* agregamos aca implements TarjetaInterface ?? */
+class MedioBoleto extends Tarjeta implements TarjetaInterface{
+    public function valorBoleto(){
+		return $this->valor/2;
+	}
 }
