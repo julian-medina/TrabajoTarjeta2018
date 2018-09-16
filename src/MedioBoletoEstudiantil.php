5e69d69f79429e31ce7653273bcf7cfcf6c381ea
Uriel Man <urielman99@gmail.com>
2018-09-15 21:55:32 -0300

    MedioBoletoEstudiantil no puede pagar si no pasan 5 min

--- src/MedioBoletoEstudiantil.php
+++ src/MedioBoletoEstudiantil.php
@@ -5,11 +5,52 @@ namespace TrabajoTarjeta;
 /* agregamos aca implements TarjetaInterface ?? */
 class MedioBoletoEstudiantil extends Tarjeta implements TarjetaInterface{
    
-	protected $tiempo_de_espera = 300; //5 minutos
+	protected $tiempoDeEspera = 300; //5 minutos
     
-	protected $fecha_pagada = NULL;
+	protected $ultimaFechaPagada = NULL;
 	
 	public function valorBoleto(){
 		return $this->valor/2;
+	}
+
+/* si pasaron 5 minutos, no se puede pagar con el medio voleto. */
+	public function pagoBoleto() {
+		if($this->ultimaFechaPagada == NULL || $this->tiempoDeEsperaCumplido()){
+			if($this->obtenerSaldo() >= $this->valorBoleto()){
+				$this->pagarBoleto();
+				return TRUE;
+			}
+			return FALSE;
+		}
+
+		return FALSE;
+	}
+
+	public function pagarBoleto(){
+
+		$this->saldo -= $this->valorBoleto();
+		$tiempoNuevo = $this->tiempo->time();
+		$this->ultimaFechaPagada = $tiempoNuevo;
+	}
+
+	public function tiempoDeEsperaCumplido(){
+
+        $ultimaFechaPagada = $this->obtenerUltimaFechaPagada();
+        $fechaActual = $this->tiempo->time();
+		$diferenciaDeFechas = $fechaActual - $ultimaFechaPagada;
+		
+        if($diferenciaDeFechas >= $this->obtenerTiempoDeEspera())
+            return TRUE;
+        
+        return FALSE;
+	}
+
+
+	public function obtenerTiempoDeEspera(){
+		return $this->tiempoDeEspera;
+	}
+
+	public function obtenerUltimaFechaPagada(){
+	  return $this->ultimaFechaPagada;
 	}
 }
